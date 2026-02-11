f80ab87e4f9b7df50fd6895c1f31a8c382a8def4
Mobin Z <mobin.zaki@students.plymouth.ac.uk>
2026-02-11 12:20:20 +0000

    feat(android): add barrel export for android components

---
+++ src/components/android/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Android-specific component infrastructure.
+ *
+ * AndroidOverlayHost creates a separate compositing layer with high elevation
+ * so all UI overlays render above native Android map views.
+ */
+export { AndroidOverlayHost } from './AndroidOverlayHost';
